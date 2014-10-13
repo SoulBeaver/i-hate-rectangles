6af3a4a77596b9d84875a85b6399e48691206e44
SoulBeaver <christian.broomfield@posteo.de>
2014-10-13 17:12:07 +0200

    Add: Project setup and initial commit.

---
+++ IHateRectangles/IHateRectangles/Program.cs
@@ -0,0 +1,20 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace IHateRectangles
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new IHateRectangles())
+                game.Run();
+        }
+    }
+#endif
+}

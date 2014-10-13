abe598df75cb047a32168fbb12e2bfdfb2b38845
SoulBeaver <christian.broomfield@posteo.de>
2014-10-13 18:40:17 +0200

    Add: Configuration file.

--- IHateRectangles/IHateRectangles/Configuration.cs
+++ IHateRectangles/IHateRectangles/Configuration.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace IHateRectangles
 {
-    public class Configuration
+    public struct Configuration
     {
-        public Vector2 InitialBallVelocity { get; private set; }
-        public Vector2 BallAcceleration { get; private set; }
-        public int BallRadius { get; private set; }
-        public Color BallColor { get; private set; }
+        public Vector2 InitialBallVelocity { get; set; }
+        public Vector2 BallAcceleration { get; set; }
+        public double BallRadius { get; set; }
+        public Color BallColor { get; set; }
 
-        public int BlocksPerColumn { get; private set; }
-        public int BlocksPerRow { get; private set; }
-        public int BlockSpacing { get; private set; }
-        public int BlockDistanceFromCeiling { get; private set; }
-        public Color BlockColor { get; private set; }
+        public int BlocksPerColumn { get; set; }
+        public int BlocksPerRow { get; set; }
+        public int BlockSpacing { get; set; }
+        public int BlockWidth { get; set; }
+        public int BlockHeight { get; set; }
+        public int BlockDistanceFromCeiling { get; set; }
+        public Color BlockColor { get; set; }
 
-        public int PaddleWidth { get; private set; }
-        public int PaddleHeight { get; private set; }
-        public int PaddleDistanceFromGutter { get; private set; }
-        public Color PaddleColor { get; private set; }
+        public int PaddleWidth { get; set; }
+        public int PaddleHeight { get; set; }
+        public int PaddleDistanceFromGutter { get; set; }
+        public Color PaddleColor { get; set; }
     }
 }
---
+++ IHateRectangles/IHateRectanglesTest/ConfigurationTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using IHateRectangles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+
+namespace IHateRectanglesTest
+{
+    [TestClass]
+    public class ConfigurationTest
+    {
+        private readonly Configuration configuration = new Configuration
+        {
+            InitialBallVelocity = new Vector2(1, 2),
+            BallAcceleration = new Vector2(10, 10),
+            BallRadius = 2.0,
+            BallColor = Color.Black,
+
+            BlocksPerColumn = 5,
+            BlocksPerRow = 5,
+            BlockSpacing = 20,
+            BlockWidth = 100,
+            BlockHeight = 20,
+            BlockDistanceFromCeiling = 100,
+            BlockColor = Color.Black,
+
+            PaddleWidth = 100,
+            PaddleHeight = 20,
+            PaddleDistanceFromGutter = 100,
+            PaddleColor = Color.Black
+        };
+
+        private readonly String json =
+            "{\"InitialBallVelocity\":{\"X\":1.0,\"Y\":2.0},\"BallAcceleration\":{\"X\":10.0,\"Y\":10.0},\"BallRadius\":2.0,\"BallColor\":{\"B\":0,\"G\":0,\"R\":0,\"A\":255},\"BlocksPerColumn\":5,\"BlocksPerRow\":5,\"BlockSpacing\":20,\"BlockWidth\":100,\"BlockHeight\":20,\"BlockDistanceFromCeiling\":100,\"BlockColor\":{\"B\":0,\"G\":0,\"R\":0,\"A\":255},\"PaddleWidth\":100,\"PaddleHeight\":20,\"PaddleDistanceFromGutter\":100,\"PaddleColor\":{\"B\":0,\"G\":0,\"R\":0,\"A\":255}}";
+
+        [TestMethod]
+        public void SerializeConfiguration_ReturnsJson()
+        {
+            var actual = JsonConvert.SerializeObject(configuration);
+
+            Assert.AreEqual(actual, json);
+        }
+
+        [TestMethod]
+        public void DeserializeConfiguration_PopulatesConfiguration()
+        {
+            var actual = JsonConvert.DeserializeObject<Configuration>(json);
+
+            Assert.AreEqual(actual, configuration);
+        }
+    }
+}
